c635194d4014307b0c1b484d400d25bfce661168
Razzorback225 <47690391+Razzorback225@users.noreply.github.com>
2021-09-06 12:14:24 +0200

    added some comments and fix functon

--- PACU/PACU_TEST_CODE/PACU_TEST_CODE.ino
+++ PACU/PACU_TEST_CODE/PACU_TEST_CODE.ino
@@ -1,13 +1,15 @@
 #define ZCD_PIN 19
 #define GC_PIN 18
 
-int delta
+int delta;
 
 void setup() {
-  // put your setup code here, to run once:
+  //Define pin mode
   pinMode(ZCD_PIN, INPUT);
   pinMode(GC_PIN, OUTPUT);
+  //Define interrupt on input pin
   attachInterrupt(digitalPinToInterrupt(ZCD_PIN), setTrackVoltage, FALLING);
+  //Init serial 
   Serial.begin(115200);
 }
 
@@ -16,13 +18,16 @@ void loop() {
   digitalWrite(GC_PIN, LOW);  
   if(Serial.available() > 0){
     String spdPerc = Serial.readStringUntil('#');
-    delta = map(spdPerc, 0, 100, 8500, 0);
+    delta = map(spdPerc.toInt(), 0, 100, 8500, 0);
+    Serial.print("Speed percentage : ");Serial.println(spdPerc);
+    Serial.print("Delay value : ");Serial.println(delta);
   }
   digitalWrite(GC_PIN, LOW);
 }
 
+//Set track voltage according to the set microDelay
 void setTrackVoltage(){
-  delayMicroseconds(volt);
+  delayMicroseconds(delta);
   digitalWrite(GC_PIN, HIGH);
   delayMicroseconds(500);
   digitalWrite(GC_PIN, LOW);
